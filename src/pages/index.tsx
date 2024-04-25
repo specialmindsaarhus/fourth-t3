c7ec4338073eb572cd77419e5778fa16d11c0b41
Mark Skovbon <mask@specialminds.dk>
2024-04-25 14:32:23 +0200

    emoji added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,7 +45,6 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="text-white">
           {
-            !!user.isSignedIn &&
             data?.map((item)=>{
               return <p key={item.id}>{item.name}</p>
             })
