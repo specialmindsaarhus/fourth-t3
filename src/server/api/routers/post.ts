f246e0fd7f011cd023756612c814e0cc2a23c60a
specialmindsaarhus <mask@specialminds.dk>
2024-08-08 09:57:33 +0200

    small changes, posibly nothiing but a Image back to img>

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,9 +1,22 @@
+import { clerkClient } from "@clerk/nextjs";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findMany()
-  })
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+    });
+
+    const users = clerkClient.users.getUserList({
+      userId: posts.map((post) => post.autherId),
+      limit: 100,
+    });
+
+    //console.log("users from clerk: ");
+    console.log("user from clerk: ", users);
+
+    return posts;
+  }),
 });
