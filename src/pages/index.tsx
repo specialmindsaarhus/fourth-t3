4ed0b19c10a828638c5620bed01cb54adfc865c3
specialmindsaarhus <mask@specialminds.dk>
2024-08-09 11:17:54 +0200

    no eslint for a line

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ const CreatePostWizard = () => {
   );
 };
 
-export default function Home(props) {
+export default function Home(props) {  //eslint-disable-line padded-blocks
   const user = useUser();
 
   const { data } = api.post.getAll.useQuery();
